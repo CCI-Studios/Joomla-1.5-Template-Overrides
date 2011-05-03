6071ac4ab159fd942a347fa9b0973cb96e066fc4
Jonathan Bennett <jbennett@ccistudios.com>
2011-05-03 12:19:58 -0400

    Initial Import

---
+++ com_user/register/default_message.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* @package   Template Overrides YOOtheme
+* @version   1.5.8 2009-11-30 14:31:42
+* @author    YOOtheme http://www.yootheme.com
+* @copyright Copyright (C) 2007 - 2009 YOOtheme GmbH
+* @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+*/
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+?>
+
+<h2>
+	<?php echo $this->escape($this->message->title); ?>
+</h2>
+
+<p>
+	<?php echo $this->escape($this->message->text); ?>
+</p>
